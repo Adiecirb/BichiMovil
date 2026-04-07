c421b2eb34bb976cd0e93bbbbcfe05c774aa4787
Adiecirb <briceidamh@comunidad.unam.mx>
2026-04-07 08:51:47 -0600

    LogingFrameNavigation

---
+++ app/src/main/java/com/example/bichimovil/LoginFragment.kt
@@ -0,0 +1,36 @@
+package com.example.bichimovil
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.bichimovil.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding !!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.registro.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
+        }
+        return inflater.inflate(R.layout.fragment_login, container, false)
+
+
+    }
+}
